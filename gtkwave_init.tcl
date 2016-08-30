1e7d2fb3696caaf203d54a6ff12d16fc1467720f
namas <gostmasterys@gmail.com>
2016-08-30 23:47:19 +0900

    scope analog don't existe anymore now it's real

--- gtkwave_init.tcl
+++ gtkwave_init.tcl
@@ -1,6 +1,6 @@
 ###################################################################
 # This TCL script automaticly load every signals into GTKwave
-# the signals that are into the analog scope are displayed in step
+# the signals that are into the real scope are displayed in step
 # mode and with colors (other than green)
 # -----------------------------------------------------------------
 # Usage: 
@@ -13,10 +13,10 @@ set nfacs [ gtkwave::getNumFacs ]
 set dumpname [ gtkwave::getDumpFileName ]
 set dmt [ gtkwave::getDumpType ]
 
-set analog_colors {Red Yellow Blue Orange Indigo}
+set real_colors {Red Yellow Blue Orange Indigo}
 
 puts "number of signals in dumpfile '$dumpname' of type $dmt: $nfacs"
-puts "special formating for signals in analog scope module"
+puts "special formating for signals in real scope module"
 
 
 gtkwave::/Edit/Insert_Analog_Height_Extension
@@ -29,16 +29,16 @@ for {set i 0} {$i < $nfacs } {incr i} {
     puts "add signal '$facname'"
     set num_added [ gtkwave::addSignalsFromList $facname ]
 
-    if { [string match {*analog.*} $facname] } {
-        puts "put it in analog format"
+    if { [string match {*real.*} $facname] } {
+        puts "put it in real format"
         gtkwave::/Edit/Data_Format/Analog/Step
-        gtkwave::/Edit/Color_Format/[lindex $analog_colors $color_choice]
+        gtkwave::/Edit/Color_Format/[lindex $real_colors $color_choice]
         gtkwave::/Edit/Insert_Analog_Height_Extension
         gtkwave::/Edit/Insert_Analog_Height_Extension
 
     } 
 
-    if {$color_choice == [llength $analog_colors]} {
+    if {$color_choice == [llength $real_colors]} {
         set color_choice 0
     } else {
         incr color_choice
